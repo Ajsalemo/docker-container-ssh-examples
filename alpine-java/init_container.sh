731335b806500208bc1ffdbf1ee2a5fe74c6acab
Anthony Salemo <ajssalemo@gmail.com>
2022-04-27 17:28:14 -0400

    added a alpine/java example

---
+++ alpine-java/init_container.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Get env vars in the Dockerfile to show up in the SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+echo "Starting SSH ..."
+/usr/sbin/sshd
+
+echo "Running startup command 'java -jar /app/ssh-0.0.1-SNAPSHOT.jar'"
+java -jar /app/ssh-0.0.1-SNAPSHOT.jar
